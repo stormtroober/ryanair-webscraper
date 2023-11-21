5426eced93243b6ab07a94224bbfd49010d1a9c5
aleshark87 <trofui@gmail.com>
2023-11-21 18:26:30 +0100

    works kinda

---
+++ FlightSearcher.py
@@ -0,0 +1,153 @@
+import csv
+from selenium import webdriver
+import subprocess
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import datetime
+from FlightURLBuilder import FlightURLBuilder
+from selenium.webdriver.common.by import By
+from datetime import datetime
+import random
+import time
+
+vpn_countries = ['Germany', 'Italy', 'Portugal', 'Spain', 'Switzerland', 'France']
+
+class FlightSearcher:
+    def __init__(self, vpn, saveCsv):
+        self.driver = webdriver.Chrome()
+        self.driver.delete_all_cookies()
+        self.vpn = vpn
+        self.saveCsv = saveCsv
+
+    def disconnect_vpn(self):
+        if self.vpn:
+            bash_script = './nordvpn_disconnect.sh'  # Replace with the actual path
+            command = f"{bash_script}"
+            result = subprocess.run(command, shell=True)
+
+    def connect_vpn(self, vpn_server):
+        if self.vpn:
+            bash_script = './nordvpn_connect.sh'  # Replace with the actual path
+            command = f"{bash_script} {vpn_server}"
+            result = subprocess.run(command, shell=True)
+
+            if result.returncode == 0:
+                print("Connected to NordVPN successfully.")
+                self.vpnStatus = True
+                return True
+            else:
+                print("Failed to connect to NordVPN.")
+                return False
+
+    def search_flights(self, origins, destinations, dates):
+        prices = {}
+        for origin in origins:
+            for destination in destinations:
+                for date in dates:
+                    flight_key = f"{origin}-{destination} on {date}"
+                    prices[flight_key] = self.get_price(origin, destination, date)
+        return prices, flight_key
+
+    def get_price(self, origin, destination, date):
+        flightBuilder = FlightURLBuilder()
+        flightBuilder.set_origin(origin)
+        flightBuilder.set_destination(destination)
+        flightBuilder.set_date_out(date)
+
+        url = flightBuilder.build_url()
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless") 
+        chrome_options.add_argument("--disable-cache")
+        chrome_options.add_argument("--disable-application-cache")
+        chrome_options.add_argument("--disable-offline-load-stale-cache")
+
+        self.driver = webdriver.Chrome(chrome_options)
+        self.driver.delete_all_cookies()
+        self.driver.get(url)
+
+        try:
+            cookie_button = WebDriverWait(self.driver, 5).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, 'cookie-popup-with-overlay__button'))
+            )
+            cookie_button.click()
+        except TimeoutException:
+            print("Cookie button not found or not clickable.")
+
+        try:
+            
+            WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "flight-card-new"))
+            )
+            price_carousel = self.driver.find_elements(By.CSS_SELECTOR, "carousel-container carousel-item")
+            price_info = self.get_price_selected(price_carousel, datetime.strptime(date, "%Y-%m-%d"))
+
+            self.driver.delete_all_cookies()
+            self.driver.quit()
+            return price_info
+        except TimeoutException:
+            return "Flight not found"
+
+    def get_price_selected(self, priceCarousel, flightDate):
+        priceSelected = None
+        for box in priceCarousel:
+            child_elements = box.find_elements(By.XPATH, "./*")
+            for element in child_elements:
+                isSelected = 'date-item--selected' in element.get_attribute('class').split()
+                if isSelected:
+                    priceSelected = box.find_element(By.CSS_SELECTOR, 'ry-price')
+                    break
+        return self.extract_price_info(priceSelected.text, flightDate)
+
+    def extract_price_info(self, text, flightDate):
+        try:
+            parts = text.split()
+            currency = parts[0]
+            amount_str = ''.join(parts[1:]).replace(',', '.')
+            amount = float(amount_str)
+            return {
+                'currency': currency,
+                'amount': amount,
+                'date': flightDate.strftime("%Y-%m-%d")
+            }
+        except Exception as e:
+            print(f"Error extracting price info: {e}")
+            return None
+
+    def close(self):
+        self.driver.quit()
+        self.disconnect_vpn()
+
+    def search_and_save_flights(self, origins, destinations, dates):
+        found = False
+        if self.vpn:
+            self.connect_vpn(random.choice(vpn_countries))
+        flight_prices, flight_key  = self.search_flights(origins, destinations, dates)
+        print(flight_prices)
+        if 'currency' not in flight_prices[flight_key]:
+            return found
+        else:
+            found = True
+        if self.saveCsv:
+            print("Saving flights data...")
+            with open('/home/aless/Documents/dataset_voli/flights.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                for key, value in flight_prices.items():
+                    if 'currency' in value:
+                        origin_destination = key.split(' on ')[0]
+                        flight_date = value['date']
+                        price_currency = f"{value['amount']}{value['currency']}"
+                        today_date = datetime.today().strftime("%Y-%m-%d %H:%M")
+
+                        writer.writerow([origin_destination, flight_date, price_currency, today_date])
+        return found
+        
+    def search_flights_with_retry(self, origin, destination, dates, max_retries):
+        for attempt in range(max_retries):
+            found = self.search_and_save_flights([origin], [destination], dates)
+            if found:
+                return True
+            time.sleep(5)
+            print(f"Attempt {attempt + 1} failed. Retrying...")
+        return False
--- main.py
+++ main.py
@@ -1,46 +1,27 @@
+import csv
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.remote.webelement import WebElement
-from UtilityMethods import get_price_selected
 from datetime import datetime
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from FlightURLBuilder import FlightURLBuilder
+import argparse
+from FlightSearcher import FlightSearcher
+    
 
 
-flightBuilder = FlightURLBuilder()
-flightBuilder.set_origin('BLQ')
-flightBuilder.set_destination('KRK')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flight Searcher")
+    # Add command-line arguments for vpn and saveCsv
+    parser.add_argument("--vpn", action="store_true", help="Enable VPN (default is False)")
+    parser.add_argument("--saveCsv", action="store_true", help="Save CSV (default is False)")
+    args = parser.parse_args()
 
-date_object = datetime(2024, 1, 15)
-flightBuilder.set_date_out(date_object.strftime("%Y-%m-%d"))
+    flight_searcher = FlightSearcher(vpn=args.vpn, saveCsv=args.saveCsv)
 
-driver = webdriver.Chrome()
-print("Retrieving page and deleting cookies...")
-driver.delete_all_cookies()
-driver.get(flightBuilder.build_url())
-driver.delete_all_cookies()
+    #dates = ['2024-01-08', '2024-01-13', '2024-01-15', '2024-01-20', '2024-01-22', '2024-01-27', '2024-01-29']
+    dates = ['2024-01-13']
 
-try:
-    cookie_button = driver.find_element(By.CLASS_NAME, 'cookie-popup-with-overlay__button')
-    cookie_button.click()
-except NoSuchElementException:
-    print("Cookie button not found.")
+    found = flight_searcher.search_flights_with_retry('FRL', 'KTW', dates, 3)
+    found = flight_searcher.search_flights_with_retry('KTW', 'FRL', dates, 3)
+    found = flight_searcher.search_flights_with_retry('BLQ', 'KRK', dates, 3)
+    found = flight_searcher.search_flights_with_retry('KRK', 'BLQ', dates, 3)
 
-print('Waiting for the card-price 10 seconds...')
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "flight-card-new"))
-    )
-    print("Flight found. Searching the price")
-    price_carousel = driver.find_elements(By.CSS_SELECTOR, "carousel-container carousel-item")
-
-    price_info = get_price_selected(price_carousel, date_object)
-    print(price_info)
-    driver.quit()
-except TimeoutException:
-    print("Flight not found. Try another date.")
-    driver.quit()
-
+    flight_searcher.close()
